707dd561b42c614ad7329746ff47e3855246cd87
Lisa Walters <lisacampiwalters@gmail.com>
2023-03-13 17:47:31 -0600

    added files and wrote Add method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,14 @@
 {
     public class Program
     {
+
+        public static int Add(int x, int y)
+        { 
+            return x + y;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(4, 5));
         }
     }
 }
